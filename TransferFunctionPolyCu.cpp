e75f2660cf4fbe4278ae115014bfdba8e736d169
Hasib <hasib.jhd@gmail.com>
2018-09-04 13:00:03 +0600

    Successfully tested

--- TransferFunctionPolyCu.cpp
+++ TransferFunctionPolyCu.cpp
@@ -14,26 +14,54 @@ double _Complex RefractiveIndexCu(float lambda)
     float r = 0.4822 + (lambda-1305.26)*(0.5701-0.4822)/(1458.82-1305.26); //only this one
     float im = 7.9111 + (lambda-1305.26)*(7.6694-7.9111)/(1458.82-1305.26); //only this one
 
-    return MakeComplex(r,im);
+    return r+(im)*I;
+}
+
+float _Complex** matrixMultiplication(float _Complex** mat1, float _Complex** mat2, int noOfRowsMat1, int noOfColsMat1, int noOfRowsMat2, int noOfColsMat2)
+{
+
+    float _Complex** multiMatrix;
+
+    multiMatrix=new float _Complex *[noOfRowsMat1];
+
+    for(int i=0;i<noOfRowsMat1;i++)
+    {
+        multiMatrix[i]=new float _Complex [noOfColsMat2];
+    }
+
+    //reset multiMatrix
+
+    for(int i=0;i<noOfRowsMat1;i++)
+    {
+        for(int j=0;j<noOfColsMat2;j++)
+        {
+            multiMatrix[i][j]=0+0i;
+        }
+    }
+
+    //matrix multiplication in operation
+    for(int row=0;row<noOfRowsMat1;row++)
+    {
+        for(int col=0;col<noOfColsMat2;col++)
+        {
+            for(int i=0;i<noOfColsMat1;i++)
+            {
+                multiMatrix[row][col] += mat1[row][i] * mat2[i][col];
+            }
+        }
+    }
+
+    return multiMatrix;
 }
 
 int main()
 {
-    float lambda,polyThickness,polyRefractiveIndex,**T;
+    float lambda,polyThickness,polyRefractiveIndex;
+
     lambda=1220;
     polyThickness=20000;
     polyRefractiveIndex=1.7;
     int rows=2,colms=2;
-
-    T=new float *[rows];
-
-    for(int i=0;i<rows;i++)
-    {
-        T[i]=new float [colms];
-    }
-
-    //T[0][0]=12.2;
-    //cout<<T[0][0];
 
     float _Complex nc = RefractiveIndexCu(lambda);
     cout<<creal(nc)<<" "<<cimag(nc)<<"\n";
@@ -50,8 +78,23 @@ int main()
     float _Complex t23 = 1 + r23;//complex add
     cout<<"t23 = "<<creal(t23)<<" "<<cimag(t23)<<"\n";
 
-    float T12[2][2];
-    float _Complex T2[2][2], T23[2][2];
+
+    //array declarartion  for matrix multiplication
+    float _Complex **T2, **T23, **T,**T12;
+
+    // array size definition
+   // T=new float _Complex*[rows];
+    T12=new float _Complex*[rows];
+    T2=new float _Complex*[rows];
+    T23=new float _Complex*[rows];
+
+    for(int i=0;i<rows;i++)
+    {
+        //T[i]=new float _Complex[colms];
+        T12[i]=new float _Complex[colms];
+        T2[i]=new float _Complex[colms];
+        T23[i]=new float _Complex[colms];
+    }
 
     int noOfRows=2,noOfCols=2;
 
@@ -68,12 +111,15 @@ int main()
     T12[0][1] = r12*T12[0][1];//real
     T12[1][0] = r12*T12[1][0];//real
 
+
+    cout<<"\n\n T12\n";
     for(int i=0;i<noOfRows;i++)
     {
         for(int j=0;j<noOfCols;j++)
         {
-            cout<<"i="<<i<<"j="<<j<<"T = "<<T12[i][j]<<"\n";
+            cout<<creal(T12[i][j])<<" + "<<cimag(T12[i][j])<<"i"<<"\t";
         }
+        cout<<"\n";
     }
 
 
@@ -81,25 +127,55 @@ int main()
     T2[1][1] = cexp(-I*Phi);//complex
 
 
+    cout<<"\n\n T2\n";
     for(int i=0;i<noOfRows;i++)
     {
         for(int j=0;j<noOfCols;j++)
         {
-            cout<<"i="<<i<<"j="<<j<<"T = "<<creal(T2[i][j])<<" + "<<cimag(T2[i][j])<<"i"<<"\n";
+            cout<<creal(T2[i][j])<<" + "<<cimag(T2[i][j])<<"i"<<"\t";
         }
+        cout<<"\n";
     }
 
     T23[0][1] = r23*T23[0][1];//complex r23
     T23[1][0] = r23*T23[1][0];//complex r23
 
+
+    cout<<"\n\n T23\n";
     for(int i=0;i<noOfRows;i++)
     {
         for(int j=0;j<noOfCols;j++)
         {
-            cout<<"i="<<i<<"j="<<j<<"T = "<<creal(T23[i][j])<<" + "<<cimag(T23[i][j])<<"i"<<"\n";
+            cout<<creal(T23[i][j])<<" + "<<cimag(T23[i][j])<<"i"<<"\t";
         }
+        cout<<"\n";
     }
-    //T = T12*T2*T23;//real*complex*complex
 
+    int noOfRowsMat1=2,noOfColsMat1=2,noOfRowsMat2=2,noOfColsMat2=2;
+    T=matrixMultiplication(T12,T2,noOfRowsMat1,noOfColsMat1,noOfRowsMat2,noOfColsMat2);
+
+
+    cout<<"\n\n T\n";
+    for(int i=0;i<noOfRows;i++)
+    {
+        for(int j=0;j<noOfCols;j++)
+        {
+            cout<<creal(T[i][j])<<" + "<<cimag(T[i][j])<<"i"<<"\t";
+        }
+        cout<<"\n";
+    }
+
+    T=matrixMultiplication(T,T23,noOfRowsMat1,noOfColsMat1,noOfRowsMat2,noOfColsMat2);
+
+
+    cout<<"\n\n T\n";
+    for(int i=0;i<noOfRows;i++)
+    {
+        for(int j=0;j<noOfCols;j++)
+        {
+            cout<<creal(T[i][j])<<" + "<<cimag(T[i][j])<<"i"<<"\t";
+        }
+        cout<<"\n";
+    }
     return 0;
 }
